8fb2b8dfe482f11694901d00f97955cb366dba32
mayankvaidya436 <mayankvaidya436@gmail.com>
2023-12-11 20:42:50 +0530

    data loading

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 
 function App() {
   const [movies,setMoives]=useState([])
+  const [isLoading,setIsLoading]=useState(false)
 
    async function fetchMoivesHandler(){
+    setIsLoading(false)
    const response= await fetch('https://swapi.dev/api/films/')
        const data= await response.json();
   
@@ -19,6 +21,7 @@ function App() {
         }
       })
       setMoives(transformedMoives)
+      setIsLoading(false)
   
   }
     
@@ -29,7 +32,9 @@ function App() {
         <button onClick={fetchMoivesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length===0 && <p>found no moives.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
